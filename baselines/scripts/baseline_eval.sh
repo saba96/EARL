c28a97864ae48eb5156bfabb1e98b359906c959c
Ankur Sikarwar <ankursikarwardc@gmail.com>
2025-11-03 10:01:32 -0500

    Add baseline evaluation script

---
+++ baselines/scripts/baseline_eval.sh
@@ -0,0 +1,38 @@
+SAVE_DIR=""
+
+BASELINE=(
+    "InstructPix2Pix"
+    "MagicBrush"
+    "Aurora"
+    "Omnigen"
+)
+
+python baselines_eval_on_aurora.py \
+    --save_dir $SAVE_DIR \
+    --batch_size 40 \
+    --baseline "${BASELINE[@]}"
+
+python baselines_eval_on_emuedit.py \
+    --save_dir $SAVE_DIR \
+    --batch_size 50 \
+    --baseline "${BASELINE[@]}"
+
+python baselines_eval_on_i2ebench.py \
+    --save_dir $SAVE_DIR \
+    --batch_size 50 \
+    --baseline "${BASELINE[@]}"
+
+python baselines_eval_on_magicbrush.py \
+    --save_dir $SAVE_DIR \
+    --batch_size 40 \
+    --baseline "${BASELINE[@]}"
+
+python baselines_eval_on_omniedit.py \
+    --save_dir $SAVE_DIR \
+    --batch_size 40 \
+    --baseline "${BASELINE[@]}"
+
+python baselines_eval_on_vismin.py \
+    --save_dir $SAVE_DIR \
+    --batch_size 40 \
+    --baseline "${BASELINE[@]}"
