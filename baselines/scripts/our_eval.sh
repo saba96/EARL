615e5e939a9863fc4b8c4612bcd5c0d597613779
Ankur Sikarwar <ankursikarwardc@gmail.com>
2025-11-04 20:55:11 -0500

    Add script for our model evaluation

---
+++ baselines/scripts/our_eval.sh
@@ -0,0 +1,66 @@
+MODEL_PATHS=(
+    "mair-lab/sft-simple.rl-simple-n-complex"
+)
+USE_REVISION="False"
+REVISION=(
+    ""
+)
+
+REASONING_INPUT="False"
+BATCH_SIZE=1100
+MODE="CE"
+SAVE_DIR=""
+
+python our_eval_on_aurora.py \
+    --save_dir $SAVE_DIR \
+    --batch_size $BATCH_SIZE \
+    --mode $MODE \
+    --model_paths "${MODEL_PATHS[@]}" \
+    --reasoning_input $REASONING_INPUT \
+    --revision "${REVISION[@]}" \
+    --use_revision $USE_REVISION
+
+python our_eval_on_emuedit.py \
+    --save_dir $SAVE_DIR \
+    --batch_size $BATCH_SIZE \
+    --mode $MODE \
+    --model_paths "${MODEL_PATHS[@]}" \
+    --reasoning_input $REASONING_INPUT \
+    --revision "${REVISION[@]}" \
+    --use_revision $USE_REVISION
+
+python our_eval_on_i2ebench.py \
+    --save_dir $SAVE_DIR \
+    --batch_size $BATCH_SIZE \
+    --mode $MODE \
+    --model_paths "${MODEL_PATHS[@]}" \
+    --reasoning_input $REASONING_INPUT \
+    --revision "${REVISION[@]}" \
+    --use_revision $USE_REVISION
+
+python our_eval_on_magicbrush.py \
+    --save_dir $SAVE_DIR \
+    --batch_size $BATCH_SIZE \
+    --mode $MODE \
+    --model_paths "${MODEL_PATHS[@]}" \
+    --reasoning_input $REASONING_INPUT \
+    --revision "${REVISION[@]}" \
+    --use_revision $USE_REVISION
+
+python our_eval_on_omniedit.py \
+    --save_dir $SAVE_DIR \
+    --batch_size $BATCH_SIZE \
+    --mode $MODE \
+    --model_paths "${MODEL_PATHS[@]}" \
+    --reasoning_input $REASONING_INPUT \
+    --revision "${REVISION[@]}" \
+    --use_revision $USE_REVISION
+
+python our_eval_on_vismin.py \
+    --save_dir $SAVE_DIR \
+    --batch_size $BATCH_SIZE \
+    --mode $MODE \
+    --model_paths "${MODEL_PATHS[@]}" \
+    --reasoning_input $REASONING_INPUT \
+    --revision "${REVISION[@]}" \
+    --use_revision $USE_REVISION
